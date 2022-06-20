7f5b3321b7929dda0ef07bdc140187c4cafc4ffc
Frank Johnson <frankjohnson1993@gmail.com>
2022-06-20 01:12:23 -0400

    Fix type and build errors

--- lib/markdown.ts
+++ lib/markdown.ts
@@ -1,11 +1,15 @@
-import { allDocuments } from "contentlayer/generated";
+import { allDocuments, Book, Page, Post, Project } from "contentlayer/generated";
+
+type DocumentTypesNoNowUpdates = Book | Page | Post | Project
 
 export function parseObsidianLinks(content: string): string {
     const obsidianLinkRegex = /\[\[(.+?(\|.+?)?)\]\]([\W])/g
 
     const transformedContent = content.replace(obsidianLinkRegex, (_, p1, p2, p3) => {
         const slug = p1.includes("|") ? p1.slice(0, p1.indexOf("|")) : p1
-        const url = allDocuments.find(doc => doc.url.includes(slug))?.url;
+        
+        const filteredDocs = allDocuments.filter(doc => !['NowUpdate'].includes(doc.type)) as DocumentTypesNoNowUpdates[]
+        const url = filteredDocs.find((doc: DocumentTypesNoNowUpdates) => doc.url.includes(slug))?.url;
 
         const linkText = (p2) ? p2.slice(1) : p1
 
