8b3694647457dda5caa88c40500f6068dc75eade
Frank Johnson <frankjohnson1993@gmail.com>
2022-06-21 15:19:32 -0400

    Migrate to remote submodule

--- scripts/copy-vault-assets.js
+++ scripts/copy-vault-assets.js
@@ -1,6 +1,6 @@
 var copydir = require('copy-dir');
  
-copydir.sync('./vault/_assets', './public/assets', {
+copydir.sync('./vault/public/_assets', './public/assets', {
   utimes: true,  // keep add time and modify time
   mode: true,    // keep file mode
   cover: true    // cover file when exists, default is true
